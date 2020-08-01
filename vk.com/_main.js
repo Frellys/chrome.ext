503517954dae873e65f21a5c8eb4a1a8264ea8c1
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-01 14:41:51 +0500

    Add files via upload

--- vk.com/_main.js
+++ vk.com/_main.js
@@ -3,10 +3,8 @@ if (window.location.hostname == 'vk.com') {
         msg: {
             url: 'vk.com',
             cmds: [
-                {
-                    name: 'insertCSS',
-                    file: 'vk.com/styles.css'
-                }
+                { name: 'insertCSS', file: 'vk.com/styles.css' },
+                { name: 'executeScript', file: 'vk.com/stl.remove.js' }
             ]
         }
     });
