8c1a24116d90c332a63515cabb033b1bf5e8b468
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-07 16:46:15 +0500

    Add files via upload

--- all_urls/wasd.scroll.js
+++ all_urls/wasd.scroll.js
@@ -1,7 +1,9 @@
 window.addEventListener('keydown', function (e) {
-    if (e.code == 'KeyW' && !document.hasFocus()) {
-        console.log('yee');
+    if (e.code == 'KeyW' && document.querySelector('*:focus') == null) {
+        console.log(e);
         //console.log(document.hasFocus());
+        //console.log(document.querySelector('input:focus'));
+        //console.log(document.querySelector('*:focus'));
     }
     //console.log(e);
 }, false);
