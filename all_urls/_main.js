c14c0b2ed154b107a6e255fd37b2405908debe8c
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-07 16:16:43 +0500

    Add files via upload

---
+++ all_urls/_main.js
@@ -0,0 +1,9 @@
+chrome.runtime.sendMessage({
+    msg: {
+        url: 'vk.com',
+        cmds: [
+            { name: 'insertCSS', file: 'vk.com/styles.css' },
+            { name: 'executeScript', file: 'vk.com/stl.remove.js' }
+        ]
+    }
+});
