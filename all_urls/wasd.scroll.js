4479c93ab84e5889b9c0d307e2938b3ffd0dfd19
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-07 21:49:17 +0500

    Add files via upload

--- all_urls/wasd.scroll.js
+++ all_urls/wasd.scroll.js
@@ -1,24 +1,18 @@
+// vertical(scrollY) axis
 window.addEventListener('keydown', function (e) {
     if ((e.code == 'KeyW' || e.code == 'KeyS') && document.querySelector('*:focus') == null) {
-        //if (window.scrollY > 0) {
-        //    if (e.code == 'KeyW') {
-        //        //window.scrollBy(0, -100);
-        //        window.scrollTo({
-        //            top: window.scrollY - 200,
-        //            behavior: "smooth"
-        //        });
-        //        //window.scrollY -= 1;
-        //    }
-        //}
-        //else this.window.screenY = 0;
-        //console.log(new KeyboardEvent('keydown', { 'code': 'ArrowUp' }));
-        //window.dispatchEvent(new KeyboardEvent('keydown', { 'code': 'ArrowUp' }));
-        //customEvent = document.createEvent('KeyEvents');
-        //customEvent.initKeyEvent('keydown', true, true, window, false,
-        //    false, false, false, 38, 0);
-        //document.dispatchEvent(customEvent);
-        ////window.scrollY
-        //console.log(e);
-        //console.log(window.scrollY);
+        if (window.scrollY > 0) {
+            if (e.code == 'KeyW') {
+                //window.scrollBy(0, -100);
+                //window.scrollTo({
+                //    top: window.scrollY - 200,
+                //    behavior: "smooth"
+                //});
+                window.scrollTo({
+                    top: window.scrollY - 200
+                });
+            }
+        }
+        else this.window.screenY = 0;
     }
 }, false);
