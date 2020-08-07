825f6043a7512788373c805d8361e361ad84659f
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-08 04:46:24 +0500

    Add files via upload

--- all_urls/_main.js
+++ all_urls/_main.js
@@ -2,7 +2,7 @@ chrome.runtime.sendMessage({
     msg: {
         url: '*',
         cmds: [
-            { name: 'executeScript', file: 'all_urls/wasd.scroll.js' }
+            { name: 'executeScript', file: 'all_urls/set.wasdScroll.js' }
         ]
     }
 });
--- vk.com/_main.js
+++ vk.com/_main.js
@@ -1,11 +1,15 @@
 if (window.location.hostname == 'vk.com') {
+    // main
     chrome.runtime.sendMessage({
         msg: {
             url: 'vk.com',
             cmds: [
-                { name: 'insertCSS', file: 'vk.com/styles.css' },
-                { name: 'executeScript', file: 'vk.com/stl.remove.js' }
+                { name: 'insertCSS', file: 'vk.com/_main.styles.css' },
+                { name: 'executeScript', file: 'vk.com/rm.stl.js' }
             ]
         }
     });
+    // friends
+    //if (window.location.pathname == '/friends') {
+    //}
 }
