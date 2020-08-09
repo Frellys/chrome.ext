d477554bfd2c09b24a63a0f07cd293973899cae6
Frellys <50921387+Frellys@users.noreply.github.com>
2020-08-09 19:35:29 +0500

    Add files via upload

--- all_urls/set.wasdScroll.js
+++ all_urls/set.wasdScroll.js
@@ -3,14 +3,7 @@ window.addEventListener('keydown', function (e) {
     if ((e.code == 'KeyW' || e.code == 'KeyS') && document.querySelector('*:focus') == null) {
         if (window.scrollY > 0) {
             if (e.code == 'KeyW') {
-                //window.scrollBy(0, -100);
-                //window.scrollTo({
-                //    top: window.scrollY - 200,
-                //    behavior: "smooth"
-                //});
-                window.scrollTo({
-                    top: window.scrollY - 200
-                });
+                // request animation frame here
             }
         }
         else this.window.screenY = 0;
