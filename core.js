4613f4beec37c6a4664702a1fead3ab65a527e08
Frellys <50921387+Frellys@users.noreply.github.com>
2020-09-09 03:02:11 +0500

    Add files via upload

--- core.js
+++ core.js
@@ -1,14 +1,17 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     request.msg.cmds.forEach(function (cmd) {
         switch (cmd.name) {
-            case 'insertCSS':
+            case 'insertCSS': {
                 chrome.tabs.insertCSS(sender.tab.id, { file: cmd.file, allFrames: true, cssOrigin: 'user' });
                 break;
-            case 'executeScript':
+            }
+            case 'executeScript': {
                 chrome.tabs.executeScript(sender.tab.id, { file: cmd.file });
                 break;
-            default:
+            }
+            default: {
                 break;
+            }
         }
     });
     //for (let cmd of request.msg.cmds) {
