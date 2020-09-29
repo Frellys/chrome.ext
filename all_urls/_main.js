f23423c3aad125faa6888b5b93a7738b78aaa20a
Frellys <50921387+Frellys@users.noreply.github.com>
2020-09-29 19:36:30 +0500

    Add files via upload

--- all_urls/_main.js
+++ all_urls/_main.js
@@ -4,7 +4,6 @@ chrome.runtime.sendMessage({
         cmds: [
             { name: 'executeScript', file: 'all_urls/get.screenshot.js' },
             { name: 'executeScript', file: 'all_urls/set.mute.js' }
-            //{ name: 'executeScript', file: 'all_urls/set.wasdScroll.js' }
         ]
     }
 });
