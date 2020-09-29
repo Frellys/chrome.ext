33e7e7d10dae531c0aba4464a6fe238e7ca8c728
Frellys <50921387+Frellys@users.noreply.github.com>
2020-09-29 21:05:22 +0500

    Add files via upload

--- all_urls/get.screenshot.js
+++ all_urls/get.screenshot.js
@@ -7,13 +7,8 @@ window.addEventListener('keyup', function (e) {
                 ]
             }
         }, function (response) {
-                let link = document.createElement('a');
-                let dt = new Date();
-                let ts =
-                    (dt.getFullYear().toString()) +
-                    (dt.getMonth() < 9 ? '0' + (dt.getMonth() + 1) : dt.getMonth() + 1).toString();
-            //link.download = window.location.hostname.toString() + '_' + Date.now() + '.png';
-                link.download = window.location.hostname.toString() + '_' + ts + '.png';
+            let link = document.createElement('a');
+            link.download = window.location.hostname.toString() + '_' + Date.now() + '.png';
             link.href = response.data;
             link.click();
         });
