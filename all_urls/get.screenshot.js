aa0bcf2b510a8c9cf2ab3b82db0257393344131f
Frellys <50921387+Frellys@users.noreply.github.com>
2020-09-30 10:29:48 +0500

    Add files via upload

--- all_urls/get.screenshot.js
+++ all_urls/get.screenshot.js
@@ -1,9 +1,7 @@
 window.addEventListener('keyup', function (e) {
     if (e.code == 'PrintScreen' && e.ctrlKey) {
-        document.body.style.overflow = 'hidden';
-        document.body.style.overflowX = 'hidden';
-        document.body.style.overflowY = 'hidden';
-        (function () {
+        document.querySelector('html').style.cssText = 'overflow: hidden !important;';
+        setTimeout(function () {
             chrome.runtime.sendMessage({
                 msg: {
                     cmds: [
@@ -20,40 +18,6 @@ window.addEventListener('keyup', function (e) {
                 link.href = response.data;
                 link.click();
             });
-        }())
-        //setTimeout(function () {
-        //    chrome.runtime.sendMessage({
-        //        msg: {
-        //            cmds: [
-        //                {
-        //                    name: 'invoke',
-        //                    function: 'screenshot',
-        //                    fullscreen: e.shiftKey
-        //                }
-        //            ]
-        //        }
-        //    }, function (response) {
-        //        let link = document.createElement('a');
-        //        link.download = window.location.hostname.toString() + '_' + Date.now() + '.png';
-        //        link.href = response.data;
-        //        link.click();
-        //    });
-        //}, 1);
-        //chrome.runtime.sendMessage({
-        //    msg: {
-        //        cmds: [
-        //            {
-        //                name: 'invoke',
-        //                function: 'screenshot',
-        //                fullscreen: e.shiftKey
-        //            }
-        //        ]
-        //    }
-        //}, function (response) {
-        //    let link = document.createElement('a');
-        //    link.download = window.location.hostname.toString() + '_' + Date.now() + '.png';
-        //    link.href = response.data;
-        //    link.click();
-        //});
+        }, 0);
     }
 }, false);
