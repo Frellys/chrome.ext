08a7a14649f170bee95a61de106f2290d1e8a1ef
Frellys <50921387+Frellys@users.noreply.github.com>
2020-09-30 12:13:51 +0500

    Add files via upload

--- all_urls/get.screenshot.js
+++ all_urls/get.screenshot.js
@@ -5,13 +5,9 @@ window.addEventListener('keyup', function (e) {
             { name: 'body' }
         ];
         tags.forEach(function (tag) {
-            tag.cssText = document.querySelector(tag.name).cssText;
+            tag.cssText = document.querySelector(tag.name).style.cssText;
+            document.querySelector(tag.name).style.cssText += (tag.cssText == '' ? '' : ' ') + 'overflow: hidden !important;';
         });
-        let defaultHTMLcssText = document.querySelector('html').style.cssText;
-        //document.querySelector('html').style.cssText += defaultHTMLcssText == '' ? 'overflow: hidden !important;' : defaultHTMLcssText + ' overflow: hidden !important;';
-        //document.querySelector('html').style.cssText += ((defaultHTMLcssText == '' ? '' : ' ') + 'overflow: hidden !important;');
-        document.querySelector('html').style.cssText = 'overflow: hidden !important;';
-        document.body.style.cssText = 'overflow: hidden !important;';
         setTimeout(function () {
             chrome.runtime.sendMessage({
                 msg: {
@@ -24,7 +20,9 @@ window.addEventListener('keyup', function (e) {
                     ]
                 }
             }, function (response) {
-                //document.querySelector('html').style.cssText = defaultHTMLcssText;
+                tags.forEach(function (tag) {
+                    document.querySelector(tag.name).style.cssText = tag.cssText;
+                });
                 let link = document.createElement('a');
                 link.download = window.location.hostname.toString() + '_' + Date.now() + '.png';
                 link.href = response.data;
