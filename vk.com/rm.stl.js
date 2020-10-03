be5912908e038838c6cdf45c2a6a6714478e72f5
Frellys <50921387+Frellys@users.noreply.github.com>
2020-10-03 13:44:46 +0000

    Add files via upload

--- vk.com/rm.stl.js
+++ vk.com/rm.stl.js
@@ -1,3 +1,5 @@
-// removes scroll-to-top sidebar
-if (document.querySelector('#stl_left') != null) document.querySelector('#stl_left').remove();
-if (document.querySelector('#stl_side') != null) document.querySelector('#stl_side').remove();
+{
+    // removes scroll-to-top sidebar
+    if (document.querySelector('#stl_left') != null) document.querySelector('#stl_left').remove();
+    if (document.querySelector('#stl_side') != null) document.querySelector('#stl_side').remove();
+}
