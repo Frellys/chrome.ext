4ab21aab7f0d4695af1d0f5817586720f0cc66b2
Frellys <50921387+Frellys@users.noreply.github.com>
2020-10-03 15:15:10 +0000

    Add files via upload

--- vk.com/pn_im.peerShortcut.js
+++ vk.com/pn_im.peerShortcut.js
@@ -1,9 +1,26 @@
 {
-    let peers = document.querySelectorAll('div._im_ui_peers_list > a > span > span._im_r_tx');
-    for (let pdx = 0; pdx < 10; pdx++) {
-        let span = document.createElement('span');
-        span.style.fontFamily = 'monospace';
-        span.innerHTML = pdx + '&nbsp;';
-        peers[pdx].prepend(span);
-    }
+    //setShortcuts();
+    //function setShortcuts() {
+    //    let peers = document.querySelectorAll('div._im_ui_peers_list > a > span > span._im_r_tx');
+    //    for (let pdx = 0; pdx < 10; pdx++) {
+    //        let span = document.createElement('span');
+    //        span.className = 'peerNum';
+    //        span.style.fontFamily = 'monospace';
+    //        span.innerHTML = pdx + '&nbsp;';
+    //        peers[pdx].prepend(span);
+    //    }
+    //}
+    document.querySelectorAll('div._im_ui_peers_list > a > span > span._im_r_tx').forEach(function (peer, pdx) {
+        if (peer.querySelector('span.peerNum')) {
+            peer.querySelector('span.peerNum').remove();
+        }
+        if (pdx < 10) {
+            let span = document.createElement('span');
+            span.className = 'peerNum';
+            span.style.fontFamily = 'monospace';
+            span.innerHTML = pdx + '&nbsp;';
+            peer.prepend(span);
+            console.log(span);
+        }
+    });
 }
