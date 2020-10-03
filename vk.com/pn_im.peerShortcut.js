9e887cfd9d8a6e260d82c3a13a6697e349b293b7
Frellys <50921387+Frellys@users.noreply.github.com>
2020-10-03 15:17:33 +0000

    Add files via upload

--- vk.com/pn_im.peerShortcut.js
+++ vk.com/pn_im.peerShortcut.js
@@ -10,17 +10,20 @@
     //        peers[pdx].prepend(span);
     //    }
     //}
-    document.querySelectorAll('div._im_ui_peers_list > a > span > span._im_r_tx').forEach(function (peer, pdx) {
-        if (peer.querySelector('span.peerNum')) {
-            peer.querySelector('span.peerNum').remove();
-        }
-        if (pdx < 10) {
-            let span = document.createElement('span');
-            span.className = 'peerNum';
-            span.style.fontFamily = 'monospace';
-            span.innerHTML = pdx + '&nbsp;';
-            peer.prepend(span);
-            console.log(span);
-        }
-    });
+    setShortcuts();
+    function setShortcuts() {
+        document.querySelectorAll('div._im_ui_peers_list > a > span > span._im_r_tx').forEach(function (peer, pdx) {
+            if (peer.querySelector('span.peerNum')) {
+                peer.querySelector('span.peerNum').remove();
+            }
+            if (pdx < 10) {
+                let span = document.createElement('span');
+                span.className = 'peerNum';
+                span.style.fontFamily = 'monospace';
+                span.innerHTML = pdx + '&nbsp;';
+                peer.prepend(span);
+                console.log(span);
+            }
+        });
+    }
 }
