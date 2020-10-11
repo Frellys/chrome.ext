50ab94e561218093716ae64a0a74c3b8eb17cce6
Frellys <50921387+Frellys@users.noreply.github.com>
2020-10-11 17:55:23 +0000

    Add files via upload

--- vk.com/show.ID.js
+++ vk.com/show.ID.js
@@ -1,14 +1,6 @@
 {
-    //let profile = document.querySelector('div#profile.profile_content');
-    //let id = document.querySelector('link[href*="https://m.vk.com/id"]');
-    //if (id && profile) {
-    //    let div = document.createElement('div');
-    //    div.innerHTML = id.href.toString().split('/id')[1];
-    //    document.querySelector('div#profile_short').prepend(div);
-    //}
-    let profile = document.querySelector('div#profile.profile_content');
-    if (profile) {
-        let id = parseInt([...document.querySelectorAll('script')].pop().innerText.split('{"user_id":')[1].split(',')[0]);
+    if (document.querySelector('div#profile.profile_content')) {
+        let id = parseInt(document.querySelector('body > script:last-of-type').innerText.split('{"user_id":')[1].split(',')[0]);
         let div = document.createElement('div');
         div.style.fontFamily = 'monospace';
         div.innerHTML = 'id: ' + id;
