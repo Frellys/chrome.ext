f884a695d940c43718ae99914b75316a1ed24e8a
Frellys <50921387+Frellys@users.noreply.github.com>
2020-12-19 02:06:38 +0500

    Delete function.mute.js

--- function.mute.js
+++
@@ -1,8 +0,0 @@
-function mute(sendResponse) {
-    chrome.tabs.query({}, function (tabs) {
-        tabs.forEach(function (tab) {
-            chrome.tabs.update(tab.id, { muted: !core.muted });
-        });
-        core.muted = !core.muted;
-    });
-}
