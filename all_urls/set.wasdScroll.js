f27a64660447dba28d74ce15d16723ffbe68d646
Frellys <50921387+Frellys@users.noreply.github.com>
2020-12-19 02:44:18 +0500

    Delete set.wasdScroll.js

--- all_urls/set.wasdScroll.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('keydown', function (e) {
-    if ((e.code == 'KeyW' || e.code == 'KeyS') && document.querySelector('*:focus') == null) {
-        if (window.scrollY > 0) {
-            if (e.code == 'KeyW') {
-                // request animation frame here
-            }
-        }
-    }
-}, false);
