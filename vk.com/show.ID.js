dfdf5b51253371591f4f7a81538f12301eca29c5
Frellys <50921387+Frellys@users.noreply.github.com>
2020-12-28 19:53:05 +0500

    Delete show.ID.js

--- vk.com/show.ID.js
+++
@@ -1,9 +0,0 @@
-{
-    if (document.querySelector('div#profile.profile_content')) {
-        let id = parseInt(document.querySelector('body > script:last-of-type').innerText.split('{"user_id":')[1].split(',')[0]);
-        let div = document.createElement('div');
-        div.style.fontFamily = 'monospace';
-        div.innerHTML = 'id: ' + id;
-        document.querySelector('div#profile_short').prepend(div);
-    }
-}
