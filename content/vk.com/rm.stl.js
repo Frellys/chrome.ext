665f470d6141c366f1b1cb50482edb5c50fa3b38
Frellys <50921387+Frellys@users.noreply.github.com>
2022-02-13 14:37:53 +0500

    Delete rm.stl.js

--- content/vk.com/rm.stl.js
+++
@@ -1,8 +0,0 @@
-/**
- * removes scroll-to-top sidebar
- * @return {void}
- */
-(function () {
-    document.querySelector('#stl_left')?.remove();
-    document.querySelector('#stl_side')?.remove();
-})();
