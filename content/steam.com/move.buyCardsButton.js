42df14f67a6c0ef49ff8a1052397f6a7ad1904d5
Frellys <50921387+Frellys@users.noreply.github.com>
2023-05-11 17:51:36 +0500

    Add files via upload

---
+++ content/steam.com/move.buyCardsButton.js
@@ -0,0 +1,2 @@
+const btn = document.querySelector('div.badge_cards_to_collect > div.gamecards_inventorylink > a');
+document.querySelector('div.badge_detail_tasks > div.gamecards_inventorylink').appendChild(btn);
